f9da0fe8516199726f54747e81348e37dd037aef
ThriveTrader <156680023+ThriveTrader@users.noreply.github.com>
2024-01-20 22:20:06 +0530

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -6,21 +6,21 @@ import { CalendarDaysIcon, EllipsisVerticalIcon, ExclamationCircleIcon, EyeIcon,
 const Card = () => {
   return (
       <div class="relative flex h-full transform overflow-hidden items-end border-2 border-black rounded-xl bggg transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
-        <div class="p-4 text-white !pt-0 mt-6  mb-6 ml-4 mr-4 mkk border rounded-xl border-white transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+        <div class="p-4 text-white ccc !pt-0 mt-6  mb-6 ml-4 mr-4 mkk border rounded-xl border-white transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
           <GlobeEuropeAfricaIcon width={40} className="mt-6"  />
 
           <h2 class="mt-4 text-xl font-medium sm:text-2xl">
             Go around the world
           </h2>
         </div>
-        <div class="p-4 !pt-0 mt-6 text-white  mb-6 ml-4 mr-4 mkk border rounded-xl border-white transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+        <div class="p-4 !pt-0 mt-6 ccc text-white  mb-6 ml-4 mr-4 mkk border rounded-xl border-white transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
           <CalendarDaysIcon width={40} className="mt-6"  />
 
           <h2 class="mt-4 text-xl font-medium sm:text-2xl">
             Go around the world
           </h2>
         </div>
-        <div class="p-4 !pt-0 mt-6 mb-6 ml-4 text-white mr-4 mkk border rounded-xl border-white transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
+        <div class="p-4 !pt-0 mt-6 ccc mb-6 ml-4 text-white mr-4 mkk border rounded-xl border-white transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
           <TvIcon width={40} className="mt-6" />
 
           <h2 class="mt-4 text-xl font-medium sm:text-2xl">
@@ -35,7 +35,7 @@ const Card = () => {
           </h2>
         </div>
 
-        <div class="absolute p-4 opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
+        <div class="absolute p-4 ccc opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
           <h3 class="mt-4 text-xl font-medium sm:text-2xl">
             Go around the world
           </h3>
